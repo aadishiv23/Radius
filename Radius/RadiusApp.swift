7bdefd5f605ac5ad743a0ee4595c19274ebd408e
aadishiv <aadishiv@outlook.com>
2024-04-05 15:24:05 -0400

    Initial

---
+++ Radius/RadiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadiusApp.swift
+//  Radius
+//
+//  Created by Aadi Shiv Malhotra on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RadiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
