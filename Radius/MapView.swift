785e0ce8bb2e44d5ce85f510f3683681d2b2e8b5
aadishiv <aadishiv@outlook.com>
2024-04-06 01:52:31 -0400

    permission stuff and viewmodel

--- Radius/MapView.swift
+++ Radius/MapView.swift
@@ -8,52 +8,65 @@
 import SwiftUI
 import MapKit
 
+final class MapViewModel: ObservableObject {
+    
+    var locationManager: CLLocationManager? // Optional, as user can turn off location service for their whole phone - have to make sure its on
+    
+    func checkIfLocationServiceEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        }
+        else {
+            print("Show user alert and ask to turn on")
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        // sue location manager in this scope
+        guard let locationManager = locationManager else { return }
+        
+        // check for cases
+        
+        switch locationManager.authorizationStatus {
+        
+            case .notDetermined: // Ask for permission
+                locationManager.requestAlwaysAuthorization()
+            case .restricted:
+                print("Your locatoin suage is likely restricted due to parental control") // sometimes location restricted due to personal
+            case .denied:
+                print("You have denied the usage of locatoin. Please go into locations and fix")
+            case .authorizedAlways, .authorizedWhenInUse:
+                break
+            @unknown default:
+                break
+        }
+        
+        
+    }
+    
+    
+    
+}
+
 struct ContentView: View {
-    var body: some View {
+    /*var body: some View {
         VStack {
             NavigationView {
                 MapCardView()
             }
-            
-            /*List {
-                HStack {
-                    Image(systemName: "person")
-                    Text("Person 1")
-                    
-                    Spacer()
-                    
-                    Text("1")
-                }
-                
-                HStack {
-                    Image(systemName: "person")
-                    Text("Person 2")
-                    
-                    Spacer()
-                    
-                    Text("2")
-                }
-                
-                HStack {
-                    Image(systemName: "person")
-                    Text("Person 3")
-                    
-                    Spacer()
-                    
-                    Text("3")
-                }
-            }*/
         }
-    }
-    /*@State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.33516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    }*/
+    @StateObject private var viewModel = MapViewModel()
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.33516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
     
     var body: some View {
         Map(coordinateRegion: $region, showsUserLocation: true)
             .ignoresSafeArea() // remove top and bottom white bar
-    }*/
+    }
 }
 
-struct MapCardView: View {
+/*struct MapCardView: View {
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 37.8199, longitude: -122.4783),
         span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
@@ -84,7 +97,7 @@ struct FullScreenMapView: View {
         Map(coordinateRegion: $region, showsUserLocation: true)
             .ignoresSafeArea()
     }
-}
+}*/
 
 
 #Preview {
