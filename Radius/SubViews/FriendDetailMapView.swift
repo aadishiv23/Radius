3354134c0aebaf3b16d41c2685edbae2ca6e02af
aadishiv <aadishiv@outlook.com>
2024-06-09 21:11:10 -0500

    map toast

--- Radius/SubViews/FriendDetailMapView.swift
+++ Radius/SubViews/FriendDetailMapView.swift
@@ -11,43 +11,121 @@ import MapKit
 
 struct FriendDetailMapView: View {
     var friend: Profile
-    
-    var body: some View {
-        
-        if #available(iOS 17.0, *) {
-            Map(
-                initialPosition: MapCameraPosition.automatic,
-                bounds: MapCameraBounds(centerCoordinateBounds: MKCoordinateRegion(
-                    center: CLLocationCoordinate2D(latitude: friend.latitude, longitude: friend.longitude),
-                    span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-                )),
-                interactionModes: MapInteractionModes.all
-            ) {
-                ForEach(friend.zones) { zone in
-                    MapCircle(MKCircle(center: CLLocationCoordinate2D(latitude: zone.latitude, longitude: zone.longitude), radius: zone.radius))
-                        .foregroundStyle(.blue.opacity(0.2))
-                        .stroke(Color.blue.opacity(0.5), lineWidth: 5)
-                    
-                    Annotation(friend.full_name, coordinate: CLLocationCoordinate2D(latitude: friend.latitude, longitude: friend.longitude)) {
-                        Circle()
-                            .foregroundStyle(LinearGradient(colors: [.red, .pink, .blue], startPoint: .leading, endPoint: .trailing))
-                            .frame(width: 20, height: 20)
-                            .overlay {
-                                Text(friend.full_name.prefix(1))
-                                    .fontWeight(.bold)
-                                    .foregroundStyle(Color.white)
-                                //RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+    @State private var showOverlay = false
+    @State private var currentZoneIndex = 0
 
-                            }
+
+    var body: some View {
+        ZStack {
+            if #available(iOS 17.0, *) {
+                Map(
+                    initialPosition: MapCameraPosition.automatic,
+                    bounds: MapCameraBounds(centerCoordinateBounds: MKCoordinateRegion(
+                        center: CLLocationCoordinate2D(latitude: friend.latitude, longitude: friend.longitude),
+                        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+                    )),
+                    interactionModes: MapInteractionModes.all
+                ) {
+                    ForEach(friend.zones) { zone in
+                        MapCircle(MKCircle(center: CLLocationCoordinate2D(latitude: zone.latitude, longitude: zone.longitude), radius: zone.radius))
+                            .foregroundStyle(.blue.opacity(0.2))
+                            .stroke(Color.blue.opacity(0.5), lineWidth: 5)
+                        
+                        Annotation(friend.full_name, coordinate: CLLocationCoordinate2D(latitude: friend.latitude, longitude: friend.longitude)) {
+                            Circle()
+                                .foregroundStyle(LinearGradient(colors: [.red, .pink, .blue], startPoint: .leading, endPoint: .trailing))
+                                .frame(width: 20, height: 20)
+                                .overlay {
+                                    Text(friend.full_name.prefix(1))
+                                        .fontWeight(.bold)
+                                        .foregroundStyle(Color.white)
+                                    //RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+                                    
+                                }
+                                .onTapGesture {
+                                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.5)) {
+                                        showOverlay.toggle()
+                                   }
+                                }
+                        }
                     }
                 }
+                .mapStyle(.standard(pointsOfInterest: .excludingAll))
+            } else {
+                // Fallback on earlier versions
+                OldFriendDetailMapView(friend: friend)
             }
-            .mapStyle(.standard(pointsOfInterest: .excludingAll))
-        } else {
-            // Fallback on earlier versions
-            OldFriendDetailMapView(friend: friend)
+            
+            if showOverlay {
+                SwipeableOverlay(friend: friend, currentZoneIndex: $currentZoneIndex)
+                    .transition(.move(edge: .bottom))
+                    .animation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.5))
+            }
+//            if showOverlay {
+//                VStack {
+//                    Spacer()
+//                    
+//                    Text(friend.full_name)
+//                        .font(.title)
+//                        .padding()
+//                        .frame(maxWidth: UIScreen.main.bounds.width * 0.9)
+//                        .background(Color.white.opacity(0.8))
+//                        .foregroundColor(.black)
+//                        .cornerRadius(10)
+//                        .shadow(radius: 5)
+//                        .padding(.bottom, 25)
+//                }
+//                .transition(.move(edge: .bottom))
+//                .animation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.5), value: showOverlay)
+//            }
         }
-        
+    }
+}
+
+struct SwipeableOverlay: View {
+    var friend: Profile
+    @Binding var currentZoneIndex: Int
+    @State private var translation: CGFloat = 0
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            Text("Zone \(currentZoneIndex + 1): \(friend.zones[currentZoneIndex].name ?? "Unnamed")")
+                .font(.title)
+                .padding()
+                .frame(maxWidth: UIScreen.main.bounds.width * 0.9)
+                .background(Color.white.opacity(0.8))
+                .foregroundColor(.black)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .padding(.bottom, 25)
+                .offset(x: translation)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            self.translation = value.translation.width
+                        }
+                        .onEnded { value in
+                            let threshold: CGFloat = 50
+                            if value.translation.width < -threshold {
+                                withAnimation(.spring()) {
+                                    currentZoneIndex = (currentZoneIndex + 1) % friend.zones.count
+                                    translation = 0
+                                }
+                            } else if value.translation.width > threshold {
+                                withAnimation(.spring()) {
+                                    currentZoneIndex = (currentZoneIndex - 1 + friend.zones.count) % friend.zones.count
+                                    translation = 0
+                                }
+                            } else {
+                                withAnimation(.spring()) {
+                                    translation = 0
+                                }
+                            }
+                        }
+                )
+        }
     }
 }
 
