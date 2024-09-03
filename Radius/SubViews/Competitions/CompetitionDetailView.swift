346f147072154ff943a69b071a2ff8484956f954
aadishiv <aadishiv@outlook.com>
2024-09-03 16:41:13 -0400

    Created CompetionDetailView and VM for InfoView

---
+++ Radius/SubViews/Competitions/CompetitionDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  CompetitionDetailView.swift
+//  Radius
+//
+//  Created by Aadi Shiv Malhotra on 9/2/24.
+//
+
+import Foundation
---
+++ Radius/SubViews/Competitions/CompetitionDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CompetitionDetailViewModel.swift
+//  Radius
+//
+//  Created by Aadi Shiv Malhotra on 9/2/24.
+//
+
+import Foundation
