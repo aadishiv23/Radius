181bf8b9b4962b720b00233bee4e10e3e58864bd
aadishiv <aadishiv@outlook.com>
2024-09-12 20:55:46 -0400

    added groupService

--- Radius/Services/GroupService.swift
+++ Radius/Services/GroupService.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import Supabase
+import CryptoKit
+
+class GroupService {
+    private let supabaseClient: SupabaseClient
+    
+    init(supabaseClient: SupabaseClient) {
+        self.supabaseClient = supabaseClient
+    }
+    
+    // Create Group
+    func createGroup(name: String, description: String?, password: String) async throws {
+        let hashedPassword = SHA256.hash(data: Data(password.utf8)).compactMap { String(format: "%02x", $0) }.joined()
+        try await supabaseClient
+            .from("groups")
+            .insert([
+                "name": name,
+                "description": description ?? "",
+                "password": hashedPassword
+            ])
+            .execute()
+    }
+    
+    // Fetch User Groups
+   func fetchUserGroups(for userId: UUID) async throws -> [Group] {
+       let groupMembers: [GroupMember] = try await supabaseClient
+           .from("group_members")
+           .select("*")
+           .eq("profile_id", value: userId.uuidString)
+           .execute()
+           .value
+
+       let groupIDs = groupMembers.map { $0.group_id.uuidString }
+       return try await supabaseClient
+           .from("groups")
+           .select("*")
+           .in("id", values: groupIDs)
+           .execute()
+           .value
+   }
+}
