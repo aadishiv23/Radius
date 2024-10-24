823eb0c3c8a397cceb8f1c421eb0ced78bf5458f
aadishiv <aadishiv@outlook.com>
2024-10-24 00:01:27 -0400

    remove caching

--- Radius/Repositories/GroupsRepository.swift
+++ Radius/Repositories/GroupsRepository.swift
@@ -11,7 +11,7 @@ import Supabase
 // TODO: make published
 class GroupsRepository: ObservableObject {
     @Published var groups: [Group] = []
-    private var cache: [UUID: CachedData<[Group]>] = [:] // In-memory cache for groups
+    // private var cache: [UUID: CachedData<[Group]>] = [:] // In-memory cache for groups
     private var cacheExpiration: TimeInterval = 300 // 5 minutes
     private let groupService: GroupService
 
@@ -22,12 +22,12 @@ class GroupsRepository: ObservableObject {
     /// Fetch groups from cache or Supabase
     func fetchGroups(for userId: UUID) async throws -> [Group] {
         do {
-            if let cachedData = cache[userId], !isCacheExpired(cachedData.timestamp) {
-                return cachedData.data
-            }
+//            if let cachedData = cache[userId], !isCacheExpired(cachedData.timestamp) {
+//                return cachedData.data
+//            }
 
             let fetchedGroups = try await groupService.fetchUserGroups(for: userId)
-            cache[userId] = CachedData(data: fetchedGroups, timestamp: Date())
+          //  cache[userId] = CachedData(data: fetchedGroups, timestamp: Date())
 
             DispatchQueue.main.async {
                 self.groups = fetchedGroups
@@ -52,16 +52,17 @@ class GroupsRepository: ObservableObject {
 
     /// Invalidate cache for groups
     func invalidateGroupsCache(for userId: UUID) {
-        cache.removeValue(forKey: userId)
+      //  cache.removeValue(forKey: userId)
     }
 
     /// Invalidate all cache (for example, on logout)
     func invalidateAllGroupsCache() {
-        cache.removeAll()
+       // cache.removeAll()
     }
 
     /// Check if cache is expired
     private func isCacheExpired(_ timestamp: Date) -> Bool {
-        Date().timeIntervalSince(timestamp) > cacheExpiration
+       // Date().timeIntervalSince(timestamp) > cacheExpiration
+        return false
     }
 }
